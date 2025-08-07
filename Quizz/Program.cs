cc73149bed0cbca91d092870a8b08a87d6ba19cb
labsfiap <labsfiap@PA303MICRO20>
2025-08-07 11:23:43 -0300

    Add project files.

---
+++ Quizz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int acertos = 0;
+        acertos += FazerPergunta("Qual a capital do Brasil?", "brasilia");
+        Console.WriteLine("\nPróxima pergunta...\n");
+        acertos += FazerPergunta("Qual a capital do Chile?", "santiago");
+        Console.WriteLine($"\nVocê acertou {acertos} de 2 perguntas.");
+    }
+    static int FazerPergunta(string pergunta, string respostaCorreta)
+    {
+        Console.WriteLine(pergunta);
+        Console.Write("Digite sua resposta: ");
+        string resposta = Console.ReadLine().Trim().ToLower();
+        if (resposta == respostaCorreta.ToLower())
+        {
+            Console.WriteLine("Você acertou!");
+            return 1;
+        }
+        else
+        {
+            Console.WriteLine($"Você errou! A resposta correta é: {respostaCorreta}");
+            return 0;
+        }
+    }
+}
